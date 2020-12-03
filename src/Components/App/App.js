09441d007d6eb0d20222f631e654c73fe5fd30c4
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-03 14:45:48 -0800

    Created App and SearchBar Components and added css files

---
+++ src/Components/App/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { render } from 'react-dom';
+import { SearchBar } from '../Components/SearchBar/SearchBar.js'
+import { SearchResults } from '../Components/SearchResults/SearchResults.js'
+import { SearchBar } from '../Components/SearchBar/SearchBar.js'
+ 
+function App() {
+  return (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <!-- Add a SearchBar component -->
+          <div className="App-playlist">
+            <!-- Add a SearchResults component -->
+            <!-- Add a Playlist component -->
+          </div>
+        </div>
+    </div>
+  );
+}
+
+
+export default App;
