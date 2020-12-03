4b5e5411dcb8b4c2bb07cb5c295eab698324fc89
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-03 15:33:55 -0800

    Created SearchResults Component and added js and css files

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './SearchResults.css'
+
+class SearchResults extends React.Component {
+    render() {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                <!-- Add a TrackList component -->
+            </div>
+        )
+    }
+}
+
+export default SearchResults;
