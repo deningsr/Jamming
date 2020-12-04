d35623628967bcfadf59d491160c2366b286d107
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-03 16:23:07 -0800

    Added css styling

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,15 +3,25 @@ import ReactDOM from 'react-dom';
 import './Track.css';
 
 class Track extends React.Component {
+    renderAction() {
+        if (isRemoval) {
+            return <Button>-</Button>
+        } else {
+            return <Button>+</Button>
+        }
+    }
+
     render() {
         return (
-            <div class="Track">
-                <div class="Track-information">
+            <div className="Track">
+                <div className="Track-information">
     <               h3><!-- track name will go here --></h3>
                     <p><!-- track artist will go here--> | <!-- track album will go here --></p>
                 </div>
-                <button class="Track-action"><!-- + or - will go here --></button>
+                <button className="Track-action"><!-- + or - will go here --></button>
             </div>
         )
     }
 }
+
+export default Track;
