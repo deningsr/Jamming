4120d76780b6b28942d4da8c203f028e6db2993a
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-03 17:24:05 -0800

    Passed down tracks to Tracklist Component

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './TrackList.css'
+import Track from '../Track/Track.js'
 
 class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                <!-- You will add a map method that renders a set of Track components  -->
+                { this.props.tracks.map(track => {
+                    return <Track track={track} key={track-id}/>
+                }) }
             </div>
         )
     }
