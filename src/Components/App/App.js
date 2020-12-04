777d853be83556510012469bb959f7a2b6192c70
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-03 17:25:20 -0800

    Updated App.js html

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,20 +5,27 @@ import { SearchBar } from '../Components/SearchBar/SearchBar.js'
 import { SearchResults } from '../Components/SearchResults/SearchResults.js'
 import { SearchBar } from '../Components/SearchBar/SearchBar.js'
  
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchResults: [name, artist, album, id] };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div class="App">
           <!-- Add a SearchBar component -->
-          <div className="App-playlist">
-            <!-- Add a SearchResults component -->
+          <div class="App-playlist">
+            <SearchResults searchResults={this.state.searchResults}/>
             <!-- Add a Playlist component -->
           </div>
         </div>
-    </div>
-  );
-}
-
+      </div>
+    );
+  };
+};
 
 export default App;
+
