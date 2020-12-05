9a58376ea3d36b7c37ca1b94c474636f7759bf64
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-05 15:39:52 -0800

    Passed props to Tracklist Component

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './SearchResults.css'
+
+import './SearchResults.css';
+import TrackList from '../TrackList/TrackList.js';
 
 class SearchResults extends React.Component {
     render() {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                <!-- Add a TrackList component -->
+                <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} isRemoval={false}/>
             </div>
         )
     }
