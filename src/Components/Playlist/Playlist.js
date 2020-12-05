e540028845af6658b8f928a82154d27393d2233f
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-04 16:53:08 -0800

    imported necessary components

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { TrackList} from '../Components/TrackList.js';
-import './Playlist.css'
+import TrackList from '../TracList/TrackList.js';
+import './Playlist.css';
 
 class Playlist extends React.Component {
     render() {
         return (
             <div className="Playlist">
                 <input defaultValue={'New Playlist'}/>
-                <!-- Add a TrackList component -->
+                <TrackList />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
