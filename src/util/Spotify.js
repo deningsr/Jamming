563257b9402627697dc3fcbacf426faf5d635085
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-06 15:28:06 -0800

    Updated accessUrl variable

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -22,20 +22,20 @@ const Spotify = {
             window.history.pushState('Access Token', null, '/');
             return accessTokenMatch;
         } else {
-            const accessUrl = `https://accounts.spotify.com/authorize?
-            client_id=${clientId}&response_type=token&
-            scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+
             window.location = accessUrl;
         }
     },
 
     search(term) {
         const accessToken = Spotify.getAccessToken();
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, 
-        { headers: {
-            Authorization: `Bearer ${accessToken}`
-        }}).then(response => {
-            return response.json()
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, { 
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            return response.json();
         }).then(jsonResponse => {
             if (!jsonResponse.tracks) {
                 return [];
@@ -43,8 +43,8 @@ const Spotify = {
             return jsonResponse.tracks.items.map(track => ({
                 id: track.id,
                 name: track.name,
-                artist: track.artist,
-                album: track.album,
+                artists: track.artists[0].name,
+                album: track.album.name,
                 uri: track.uri
             }));
         });
