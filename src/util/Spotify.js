be6b964162acb57a57561a551f64b1e34992d129
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-06 13:45:58 -0800

    Added search method to Spotify module

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,10 +1,12 @@
+import SearchBar from "../Components/SearchBar/SearchBar";
+
 const clientId = "2f2d17c2146f4b5eaea174b6574bda10";
 const redirectUri = "http://localhost:3000/";
 
 let accessToken = '';
 
 const Spotify = {
-    getAccessTokem() {
+    getAccessToken() {
         if (accessToken) {
             return accessToken;
         }
@@ -19,8 +21,35 @@ const Spotify = {
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
             return accessTokenMatch;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?
+            client_id=${clientId}&response_type=token&
+            scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
         }
+    },
+
+    search() {
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, 
+        { headers: {
+            Authorization: `Bearer ${accessToken}`
+        }}).then(response => {
+            return response.json()
+        }).then(jsonResponse => {
+            if (!jsonResponse.tracks) {
+                return [];
+            }
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artist,
+                album: track.album,
+                uri: track.uri
+            }));
+        })
     }
+
 }
 
 export default Spotify;
